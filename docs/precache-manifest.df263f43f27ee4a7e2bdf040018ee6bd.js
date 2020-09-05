edc4cd09e7374f75dd00c0f57df25900e24e7e74
Erik <mortensenerikm@gmail.com>
2020-09-04 21:28:12 -0300

    Renombrando a docs

---
+++ docs/precache-manifest.df263f43f27ee4a7e2bdf040018ee6bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1782c04e7e8ee7a1d529b42558b4badd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "061936f0b6ff195dd82f",
+    "url": "/static/css/main.1272b915.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061936f0b6ff195dd82f",
+    "url": "/static/js/main.fe5fb066.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
